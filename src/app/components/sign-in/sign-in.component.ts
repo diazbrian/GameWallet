a107465a3062e7cfe7562733beb224ec1ede6e9c
Brian Diaz <diazbrian1666@gmail.com>
2024-06-29 13:56:20 -0300

    feat: add addUser method

--- src/app/components/sign-in/sign-in.component.ts
+++ src/app/components/sign-in/sign-in.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
+import { User } from '../../interfaces/user';
+import { UserService } from '../../services/user.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-sign-in',
@@ -6,5 +10,34 @@ import { Component } from '@angular/core';
   styleUrl: './sign-in.component.css'
 })
 export class SignInComponent {
+  username: string = '';
+  password: string = '';
+  confirmPassword: string = '';
 
+  constructor(private toastr: ToastrService, private _userServices: UserService, private router: Router) { }
+
+  ngOnInit(): void { }
+
+  addUser() {
+    // Validacion
+    if (this.username == '' || this.password == '' || this.confirmPassword == '') {
+      this.toastr.error('Llena todos los campos', 'Error');
+      return;
+    }
+    if (this.password != this.confirmPassword) {
+      this.toastr.error('Las contraseñas no coinciden', 'Errror');
+      return;
+    }
+
+    // Crear usuario
+    const user: User = {
+      username: this.username,
+      password: this.password
+    }
+
+    this._userServices.signIn(user).subscribe(data => {
+      this.toastr.success(`Usuario  ${this.username} registrado`, 'Éxito');
+      this.router.navigate(['/login']);
+    })
+  }
 }
