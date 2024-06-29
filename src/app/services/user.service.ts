fd59b2051b42789cfba0b46e6ce967beb61edf09
Brian Diaz <diazbrian1666@gmail.com>
2024-06-29 13:57:35 -0300

    feat: add service

---
+++ src/app/services/user.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import { environment } from '../../environments/environment';
+import { User } from '../interfaces/user';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+  // url de la app backend
+  private myAppUrl: string;
+  // url de la api
+  private myApiUrl: string;
+
+  constructor(private http: HttpClient) {
+    this.myAppUrl = environment.endpoint; // file:environment.ts
+    this.myApiUrl = '/api/users/';
+  }
+
+  // metodo para registrar usuario
+  signIn(user: User): Observable<any> {
+    return this.http.post(`${this.myAppUrl}${this.myApiUrl}`, user);
+  }
+}
