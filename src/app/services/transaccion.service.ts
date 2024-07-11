7aa8d599d9c44ecfc1caf62782c3926f7cf1961c
Gabriel <gh0736340@gmail.com>
2024-07-11 16:55:23 -0300

    feat: agrego componentes de transacciones

--- src/app/services/transaccion.service.ts
+++ src/app/services/transaccion.service.ts
@@ -2,24 +2,33 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
+export interface Tarjeta {
+  card_number: number;
+}
+
 export interface Transaccion {
   id: number;
   monto: number;
   tipo: string;
   fecha: Date;
   estado: string;
-  tarjeta: any; // Ajusta este tipo según tu modelo de Tarjeta
+  tarjeta: Tarjeta;
 }
 
 @Injectable({
   providedIn: 'root'
 })
 export class TransaccionService {
-  private apiUrl = 'http://localhost:3000/api/transaccion/'; // Cambia esto por la URL de tu API
+  private apiUrl = 'http://localhost:3000/api/transaccion';
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
   getTransacciones(): Observable<Transaccion[]> {
     return this.http.get<Transaccion[]>(this.apiUrl);
+  }
+
+  getTransaccionesPorTarjeta(cardNumber: number): Observable<Transaccion[]> {
+    const url = `${this.apiUrl}?cardNumber=${cardNumber}`;
+    return this.http.get<Transaccion[]>(url);
   }
 }
