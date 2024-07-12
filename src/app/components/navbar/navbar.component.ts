2ec3f7bb873bf05ca9985c7249e1ca8192628121
Brian Diaz <diazbrian1666@gmail.com>
2024-07-12 16:51:56 -0300

    feat: en navbar foto del usuario de google

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -10,16 +10,33 @@ import { Router } from '@angular/router';
 })
 export class NavbarComponent {
 
+  profileImage: string = 'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png';
+
   constructor(
     private _authGoogleService: AuthGoogleService,
     private toastr: ToastrService,
     private router: Router
   ) {}
 
+  ngOnInit(): void {
+    setTimeout(() => {
+      this.getProfile();
+    }, 1000);
+  }
 
   logout() {
     this._authGoogleService.logout();
     this.toastr.info('Has cerrado la sesion', 'Sesion Cerrada');
     this.router.navigate(['/login']);
   }
+
+
+  getProfile() {
+    const profile = this._authGoogleService.getProfile();
+    if (profile) {
+      console.log(profile['picture']);
+      this.profileImage = profile['picture'];
+    }
+  }
+
 }
